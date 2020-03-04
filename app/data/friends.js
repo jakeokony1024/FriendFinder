82142b1caf85d12409dddeeb8882f1205c4d8107
Jakeokony1024 <jakeokony@gmail.com>
2020-03-04 15:59:26 -0600

    updated pages with new code

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+let friendArr = [
+
+    {
+        name: "Ned Flanders",
+        photo:"https://images2.minutemediacdn.com/image/upload/c_crop,h_860,w_1529,x_54,y_0/f_auto,q_auto,w_1100/v1554826525/shape/mentalfloss/nedflanders_1692813.jpg",
+        scores: [
+            "5",
+            "5",
+            "3",
+            "3",
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Moe Szyslak",
+        photo: "https://i.pinimg.com/originals/dc/58/98/dc589844e576ad2778f4dffbdefc20cf.png",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Lisa Simpson",
+        photo: "https://www.stickpng.com/assets/images/5a0c40b85a997e1c2cea116e.png",
+        scores: [
+            "3",
+            "4",
+            "3",
+            "3",
+            "3",
+            "2",
+            "3",
+            "5",
+            "3",
+            "3"
+        ]
+    },
+    {  name: "Apu",
+        photo: "https://vignette.wikia.nocookie.net/simpsons/images/7/7d/Apu_Nahasapeemapetilon.png/revision/latest/top-crop/width/360/height/360?cb=20171027210822",
+        scores:[
+                '5',
+                '1',
+                '4',
+                '4',
+                '5',
+                '1',
+                '2',
+                '5',
+                '4',
+                '1'
+        ]
+    }
+];
+
+module.exports = friendArr;
--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+app.use(express.static(__dirname + "/app/public"))
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
